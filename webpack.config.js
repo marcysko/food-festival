d150751f2012e1c4afae9b3c6e784b12dcb0747d
Marcia Skousen <75454722+marcysko@users.noreply.github.com>
2021-06-02 09:21:02 -0700

    add optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,51 @@
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const webpack = require("webpack")
 const path = require("path");
 
 module.exports = {
-  entry: "./assets/js/script.js",
-  output: {
-    path: path.join(__dirname + "/dist"),
-    filename: "main.bundle.js"
+  entry: {
+    app: "./assets/js/script.js",
+    events: "./assets/js/events.js",
+    schedule: "./assets/js/schedule.js",
+    tickets: "./assets/js/tickets.js"
   },
+  output: {
+    filename: "[name].bundle.js",
+    path: __dirname + "/dist"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jpg$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              esModule: false,
+              name(file) {
+                return '[path][name].[ext]';
+              },
+              publicPath: function(url) {
+                return url.replace('../', '/assets/');
+              }
+            }
+          },
+          {
+            loader: 'image-webpack-loader'
+          }
+        ]
+      }
+    ]
+  },
+
   plugins: [
-  new webpack.ProvidePlugin({
-    $: "jquery",
-    jQuery: "jquery"
-  }),
-  ],
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+    })
+  ],    
   mode: "development"
 };
